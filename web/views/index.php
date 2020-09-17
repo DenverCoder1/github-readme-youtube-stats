acbda976284dc4133e74a5002e2c95033fd67571
DenverCoder1 <jonah@freshidea.com>
2020-09-17 22:56:08 +0300

    improved code readability and simplified validations

--- web/views/index.php
+++ web/views/index.php
@@ -1,15 +1,17 @@
 <?php
 
+// default parameters if not specified
 $defaults = [
     "color" => "brightgreen",
     "logo" => "youtube",
     "logoColor" => "white",
     "style" => "flat-square",
-    "label" => "YouTube%20view%20count",
+    "label" => "YouTube view count",
     "id" => "UCipSxT7a3rn81vGLw9lqRkg",
-    "key" => "[YOUR API KEY HERE]"
+    "key" => "[YOUR API KEY HERE]" // you can put your API key here if you deploy it yourself
 ];
 
+// get the contents of a URL
 function curl_get_contents($url)
 {
     $ch = curl_init();
@@ -24,6 +26,7 @@ function curl_get_contents($url)
     return $response;
 }
 
+// validate parameters and return param if valid and default otherwise
 function validateParam($name, $pattern, $defaults)
 {
     if (isset($_GET[$name]) && preg_match($pattern, $_GET[$name])) {
@@ -33,22 +36,46 @@ function validateParam($name, $pattern, $defaults)
     }
 }
 
+// check for and validate parameters
 $color = validateParam("color", "/^([0-9a-fA-F]{3}|[0-9a-fA-F]{6}|[A-Za-z]+)$/", $defaults);
-$logo = validateParam("logo", "/^[A-Za-z0-9]+$/", $defaults);
+$logo = validateParam("logo", "/^[A-Za-z0-9\-]+$/", $defaults);
 $logoColor = validateParam("logoColor", "/^([0-9a-fA-F]{3}|[0-9a-fA-F]{6}|[A-Za-z]+)$/", $defaults);
 $style = validateParam("style", "/^[A-Za-z\-]+$/", $defaults);
-$label = validateParam("label", "/^[A-Za-z0-9!~ %._,\-\/:'\"|=+&()*^$#@`{}?<>\[\]]+$/", $defaults);
-$id = validateParam("id", "/^[A-Za-z0-9\-\_]+$/", $defaults);
+$label = validateParam("label", "/^[^#&?<>]+$/", $defaults);
+$id = validateParam("id", "/^[A-Za-z0-9]+$/", $defaults);
 $key = validateParam("key", "/^[A-Za-z0-9_]+$/", $defaults);
 
+// API key was not specified in URL or defaults
 if (preg_match("/^\[.*\]$/", $key)) {
     die("Your API key is missing or invalid.");
 }
 
+// set content type for output
 header('Content-type: image/svg+xml');
 
-$IMAGE_URL = "https://img.shields.io/badge/dynamic/json?color=$color&logo=$logo&logoColor=$logoColor&style=$style&label=$label&query=%24.items%5B0%5D.statistics.viewCount&url=https%3A%2F%2Fwww.googleapis.com%2Fyoutube%2Fv3%2Fchannels%3Fpart%3Dstatistics%26id%3D$id%26alt%3Djson%26key%3D$key";
+// URL for accessing the YouTube API
+$apiUrl = "https://www.googleapis.com/youtube/v3/channels?part=statistics";
+$apiUrl .= "&id=" . $id . "&alt=json&key=" . $key;
 
-$response = curl_get_contents($IMAGE_URL);
+// Query for finding view count in JSON
+$query = "$.items[0].statistics.viewCount";
 
-echo $response;
+// URL parameters
+$params = array(
+    "color" => $color,
+    "logo" => $logo,
+    "logoColor" => $logoColor,
+    "style" => $style,
+    "label" => $label,
+    "query" => $query,
+    "url" => $apiUrl
+);
+
+// build the shields.io url using the above parameters and JSON query
+$url = "https://img.shields.io/badge/dynamic/json?" . http_build_query($params);
+
+// output URL in a comment
+echo "<!-- " . $url . " -->";
+
+// get response from the URL and output its contents
+echo curl_get_contents($url);
