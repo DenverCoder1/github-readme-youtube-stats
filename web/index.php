b9a8d738ac9880a11d95d8f48bf2f54271c70680
DenverCoder1 <jonah@freshidea.com>
2020-09-17 15:21:35 +0300

    Added code for API

--- web/index.php
+++ web/index.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello";
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Github Readme Youtube Stats</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    <h1>Github Readme Youtube Stats</h1>
+    <a href="https://github.com/DenverCoder1/github-readme-youtube-stats">Read the docs</a>
+  </div>
+</body>
+
+</html>
---
+++ web/views/index.php
@@ -0,0 +1,54 @@
+<?php
+
+$defaults = [
+    "color" => "brightgreen",
+    "logo" => "youtube",
+    "logoColor" => "white",
+    "style" => "flat-square",
+    "label" => "YouTube%20view%20count",
+    "id" => "UCipSxT7a3rn81vGLw9lqRkg",
+    "key" => "[YOUR API KEY HERE]"
+];
+
+function curl_get_contents($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+function validateParam($name, $pattern, $defaults)
+{
+    if (isset($_GET[$name]) && preg_match($pattern, $_GET[$name])) {
+        return $_GET[$name];
+    } else {
+        return $defaults[$name];
+    }
+}
+
+$color = validateParam("color", "/^(#[0-9a-fA-F]{6}|[A-Za-z]+)$/", $defaults);
+$logo = validateParam("logo", "/^[A-Za-z0-9]+$/", $defaults);
+$logoColor = validateParam("logoColor", "/^(#[0-9a-fA-F]{6}|[A-Za-z]+)$/", $defaults);
+$style = validateParam("style", "/^[A-Za-z\-]+$/", $defaults);
+$label = validateParam("label", "/^[A-Za-z0-9!~ %._,\-\/:'\"|=+&()*^$#@`{}?<>\[\]]+$/", $defaults);
+$id = validateParam("id", "/^[A-Za-z0-9\-\_]+$/", $defaults);
+$key = validateParam("key", "/^[A-Za-z0-9_]+$/", $defaults);
+
+if (preg_match("/^\[.*\]$/", $key)) {
+    die("Your API key is missing or invalid.");
+}
+
+header('Content-type: image/svg+xml');
+
+$IMAGE_URL = "https://img.shields.io/badge/dynamic/json?color=$color&logo=$logo&logoColor=$logoColor&style=$style&label=$label&query=%24.items%5B0%5D.statistics.viewCount&url=https%3A%2F%2Fwww.googleapis.com%2Fyoutube%2Fv3%2Fchannels%3Fpart%3Dstatistics%26id%3D$id%26alt%3Djson%26key%3D$key";
+
+$response = curl_get_contents($IMAGE_URL);
+
+echo $response;
