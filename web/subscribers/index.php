24de34861b3a7ae14783a1bb8986950fef718990
DenverCoder1 <jonah@freshidea.com>
2020-09-18 14:08:56 +0300

    Simplified building of YouTube API query

--- web/subscribers/index.php
+++ web/subscribers/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// default parameters if not specified
+// Default parameters if not specified
 $defaults = [
     "color" => "blue",
     "logo" => "youtube",
@@ -11,7 +11,7 @@ $defaults = [
     "key" => "[YOUR API KEY HERE]" // you can put your API key here if you deploy it yourself
 ];
 
-// get the contents of a URL
+// Get the contents of a URL
 function curl_get_contents($url)
 {
     $ch = curl_init();
@@ -26,7 +26,7 @@ function curl_get_contents($url)
     return $response;
 }
 
-// validate parameters and return param if valid and default otherwise
+// Validate parameters and return param if valid and default otherwise
 function validateParam($name, $pattern, $defaults)
 {
     if (isset($_GET[$name]) && preg_match($pattern, $_GET[$name])) {
@@ -36,7 +36,7 @@ function validateParam($name, $pattern, $defaults)
     }
 }
 
-// check for and validate parameters
+// Check for and validate parameters
 $color = validateParam("color", "/^([0-9a-fA-F]{3}|[0-9a-fA-F]{6}|[A-Za-z]+)$/", $defaults);
 $logo = validateParam("logo", "/^[A-Za-z0-9\-]+$/", $defaults);
 $logoColor = validateParam("logoColor", "/^([0-9a-fA-F]{3}|[0-9a-fA-F]{6}|[A-Za-z]+)$/", $defaults);
@@ -50,17 +50,23 @@ if (preg_match("/^\[.*\]$/", $key)) {
     die("Your API key is missing or invalid.");
 }
 
-// set content type for output
+// Set the content type for output
 header('Content-type: image/svg+xml');
 
-// URL for accessing the YouTube API
-$apiUrl = "https://www.googleapis.com/youtube/v3/channels?part=statistics";
-$apiUrl .= "&id=" . $id . "&alt=json&key=" . $key;
+// URL for accessing the YouTube Data API
+$apiUrl = "https://www.googleapis.com/youtube/v3/channels?";
+// Add Youtube API parameters
+$apiUrl .= http_build_query(array(
+    "part" => "statistics",
+    "id" => $id,
+    "alt" => "json",
+    "key" => $key
+));
 
 // Query for finding subscriber count in JSON
 $query = "$.items[0].statistics.subscriberCount";
 
-// URL parameters
+// Shields.io URL parameters
 $params = array(
     "color" => $color,
     "logo" => $logo,
@@ -71,15 +77,15 @@ $params = array(
     "url" => $apiUrl
 );
 
-// build the shields.io url using the above parameters and JSON query
+// Build the Shields.io url using the above parameters and JSON query
 $url = "https://img.shields.io/badge/dynamic/json?" . http_build_query($params);
 
-// output shields.io URL in a comment (for debugging)
+// Output the Shields.io URL in a comment (for debugging)
 if (isset($_GET["showUrl"])) {
     // API key will be hidden by default in the echo'd URL
-    // to show the full URL, replace this with echo "<!-- $url -->";
+    // To show the full URL, replace this with echo "<!-- $url -->";
     echo "<!-- " . strstr($url, '%26key%3D', true) . " -->";
 }
 
-// get response from the URL and output its contents
+// Get response from the URL and output its contents
 echo curl_get_contents($url);
